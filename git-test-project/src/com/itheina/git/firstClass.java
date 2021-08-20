974595cad4192ace5a75ec3608d156fb0dbec745
zhouzhi0128 <571424571@qq.com>
2021-08-20 14:45:54 +0800

    修改FirstClass

--- git-test-project/src/com/itheina/git/firstClass.java
+++ git-test-project/src/com/itheina/git/firstClass.java
@@ -10,12 +10,13 @@ public class firstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
-        System.out.println("hello git project");
+        System.out.println("hello git project");//hello test
     }
 
     public void test() {
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 
 }
