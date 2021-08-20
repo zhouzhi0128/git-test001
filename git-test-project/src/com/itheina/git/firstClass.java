1613383fd661c8440013cfa592cd294a8b25077b
zhouzhi0128 <571424571@qq.com>
2021-08-20 13:31:32 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheina/git/firstClass.java
@@ -0,0 +1,16 @@
+package com.itheina.git;
+
+import java.sql.SQLOutput;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
+
